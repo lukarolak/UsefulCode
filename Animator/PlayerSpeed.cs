c5631f9e9e472635d2fa6b805e7fb5eff659137f
Luka Rolak <lukarolak2@gmail.com>
2017-09-29 22:33:11 +0200

    Add files via upload
    Get's player speed and feeds it to animator parameter

---
+++ Animator/PlayerSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpeed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	IEnumerator Wait()
+	{
+		Vector3 Start = gameObject.transform.position;
+		yield return new WaitForSeconds(Time.deltaTime);
+		Vector3 End = gameObject.transform.position;
+		float Speed = Vector3.Distance (Start, End) / Time.deltaTime;
+		gameObject.GetComponent<Animator> ().SetFloat("Speed", Speed);
+			
+	}
+
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine(Wait());
+	}
+}
